2c34fcc80aea827b735aa3dc0d2e2c7ec62f6e20
betterlineageci <58291749+betterlineageci@users.noreply.github.com>
2020-06-02 12:07:24 +0000

    ci - update application SimpleFileManagerPro with com.simplemobiletools.filemanager.pro

--- SimpleFileManagerPro/Android.mk
+++ SimpleFileManagerPro/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := SimpleFileManagerPro
-LOCAL_SRC_FILES := com.simplemobiletools.filemanager.pro_90.apk
+LOCAL_SRC_FILES := com.simplemobiletools.filemanager.pro_91.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
